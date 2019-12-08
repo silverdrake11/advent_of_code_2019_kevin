c0a916e7472efb6ef55d4ab0ce9e3c2a33556692
Kevin <kevinnasto@gmail.com>
2019-12-08 16:44:16 -0600

    Figured out how to use numpy for 8

--- eight.py
+++ eight.py
@@ -1,65 +1,38 @@
+import numpy as np
 import pdb
-import matplotlib
+
+
 with open('input.txt') as fh:
     text = fh.read().strip()
 
-print(text)
-
-import numpy as np
 nums = list(map(int, text))
 length = len(nums)
 
 m,n = (6, 25)
-#m,n = (2,2)
-l = length / int(m * n)
-l = int(l)
+#m,n = (2,2) # DEBUG
+num_layers = int(length / (m * n))
 
-hello = np.asarray(nums).reshape(l, n, m)
+matrices = np.asarray(nums).reshape(num_layers, m, n)
 
+max_nonzero_layer = np.count_nonzero(matrices.reshape(num_layers,(m*n)), axis=1).argmax()
+unique, counts = np.unique(matrices[max_nonzero_layer], return_counts=True)
+counts_dict = dict(zip(unique, counts))
+answer = counts_dict[1] * counts_dict[2]
+print('Part1', answer)
 
+image = np.zeros((m,n), dtype=int) + 2 # Initialize to transparent layer
+for i in reversed(range(num_layers)):
+    cur = matrices[i]
 
-nonzeros = []
-for i in range(l):
-    nz = np.count_nonzero(hello[i])
-    print(i, nz, hello[i].shape)
-    nonzeros.append(nz)
-print(max(nonzeros))
+    # Zeros where black and white pixels are
+    mask = 1 * cur
+    mask[mask == 1] = 0
+    mask[mask == 2] = 1
 
-'''print(hello[5])
+    # Zero out black and white pixels in image, then add nontransparent pixels
+    image = (image * mask) + (cur * (1-mask))
 
-#print(hello[100])
-#print(hello[100].flatten())
-unique, counts = np.unique(hello[5].flatten(), return_counts=True)
-numbers = hello[5].flatten().tolist()
-print(numbers.count(1))
-print(numbers.count(2))
-print(hello)
-print(dict(zip(unique, counts)))'''
-
-#hello[hello == 2] = 0
-#hello[hello == 1] = 0
-#hello.sum(axis=0)
-#print(hello)
-#pdb.set_trace()
-#print(hello)
-new_list = np.zeros((m,n), dtype=int) + 2
-prev = new_list.flatten().tolist()
-#print(prev)
-#new_list = [0 * len(prev)]
-for i in reversed(range(l)):
-    cur = hello[i].flatten().tolist()
-    for idx, (c,p) in enumerate(zip(cur, prev)):
-        if c == 1 or c == 0:
-            prev[idx] = c
-        else:
-            prev[idx] = p
-    #pdb.set_trace()
-print(prev)
-new = np.asarray(prev).reshape((m,n))
-print(new)
-    #print(i)
-    #print(hello[i])
+# For pretty printing
 import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
-imgplot = plt.imshow(new)
+imgplot = plt.imshow(image)
 plt.show()
