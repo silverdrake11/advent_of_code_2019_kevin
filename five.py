185e38b567b2054c6af35b96299184a1e2a56a0a
Kevin <kevinnasto@gmail.com>
2019-12-08 14:06:51 -0600

    Refactored problem 5

--- five.py
+++ five.py
@@ -15,106 +15,98 @@ for x in range(100):
         combinations.append((x,y))
 
 
-def get_params(inst):
-    inst = str(inst)
+def get_opcode(inst):
+    inst = str(inst).rjust(5,'0')
     opcode = inst[-2:]
-
-    if len(inst) == 5:
-        ret_value = inst[2], inst[1], inst[0], opcode
-
-    elif len(inst) == 4:
-        ret_value = inst[1], inst[0], 0, opcode
-
-    elif len(inst) == 3:
-        ret_value = inst[0], 0, 0, opcode
-
-    elif len(inst) == 2 or len(inst) == 1:
-        ret_value = 0, 0, 0, opcode
-
+    ret_value = inst[2], inst[1], inst[0], opcode
     return [int(x) for x in ret_value]
 
 
-def program(prog):
-    prog = [x for x in prog] # deep copy
-    i = 0
-    while True:
-        cur = prog[i]
+class Computer:
 
-        first, second, third, opcode = get_params(cur)
+    def __init__(self, prog):
+        self.prog = [x for x in prog] # deep copy
+        self.i = 0
 
-        if opcode == 99:
-            return prog[0]
+    def run(self):
+        while True:
+            cur = self.prog[self.i]
 
-        elif opcode == 1 or opcode == 2 or opcode == 7 or opcode == 8:
-            increment = 4
+            first, second, third, opcode = get_opcode(cur)
 
-            in1 = prog[i+1]
-            in2 = prog[i+2]
-            if first:
-                value1 = in1
-            else:
-                value1 = prog[in1]
-            if second:
-                value2 = in2
-            else:
-                value2 = prog[in2]
+            if opcode == 99:
+                return self.prog[0]
 
-            out = prog[i+3]
+            elif opcode == 1 or opcode == 2 or opcode == 7 or opcode == 8:
+                increment = 4
 
-            if opcode == 1:
-                prog[out] = value1 + value2
-            if opcode == 2:
-                prog[out] = value1 * value2
-            if opcode == 7:
-                if value1 < value2:
-                    prog[out] = 1
+                in1 = self.prog[i+1]
+                in2 = self.prog[i+2]
+                if first:
+                    value1 = in1
                 else:
-                    prog[out] = 0
-            if opcode == 8:
-                if value1 == value2:
-                    prog[out] = 1
+                    value1 = self.prog[in1]
+                if second:
+                    value2 = in2
                 else:
-                    prog[out] = 0
+                    value2 = self.prog[in2]
 
-        elif opcode == 3 or opcode == 4:
-            increment = 2
+                out = self.prog[self.i+3]
 
-            in1 = prog[i+1]
+                if opcode == 1:
+                    prog[out] = value1 + value2
+                if opcode == 2:
+                    prog[out] = value1 * value2
+                if opcode == 7:
+                    if value1 < value2:
+                        prog[out] = 1
+                    else:
+                        prog[out] = 0
+                if opcode == 8:
+                    if value1 == value2:
+                        prog[out] = 1
+                    else:
+                        prog[out] = 0
 
-            if opcode == 3:
-                input_value = int(input())
-                prog[in1] = input_value
+            elif opcode == 3 or opcode == 4:
+                increment = 2
+
+                in1 = prog[i+1]
+
+                if opcode == 3:
+                    input_value = int(input())
+                    prog[in1] = input_value
+                else:
+                    print(prog[in1])
+
+            elif opcode == 5 or opcode == 6:
+                increment = 3
+
+                in1 = prog[i+1]
+                in2 = prog[i+2]
+                if first:
+                    value1 = in1
+                else:
+                    value1 = prog[in1]
+                if second:
+                    value2 = in2
+                else:
+                    value2 = prog[in2]
+
+                if opcode == 5:
+                    if value1:
+                        i = value2
+                        increment = 0
+                else:
+                    assert opcode == 6
+                    if not value1:
+                        i = value2
+                        increment = 0
+
             else:
-                print(prog[in1])
+                raise Exception(opcode)
 
-        elif opcode == 5 or opcode == 6:
-            increment = 3
-
-            in1 = prog[i+1]
-            in2 = prog[i+2]
-            if first:
-                value1 = in1
-            else:
-                value1 = prog[in1]
-            if second:
-                value2 = in2
-            else:
-                value2 = prog[in2]
-
-            if opcode == 5:
-                if value1:
-                    i = value2
-                    increment = 0
-            else:
-                assert opcode == 6
-                if not value1:
-                    i = value2
-                    increment = 0
-
-        else:
-            raise Exception(opcode)
-
-        i = i + increment
+            i = i + increment
 
 
 #prog[1] = 12
